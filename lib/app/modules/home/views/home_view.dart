42d7bdb7b84d981d67099b36afa91680037ae38a
lintang permana <lntngp19@gmail.com>
2022-11-01 21:28:12 +0700

    update home view 1. informasi saldo dan isi pulsa

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -12,20 +10,37 @@ class HomeView extends GetView<HomeController> {
       appBar: AppBar(
         centerTitle: false,
         title: RichText(
-            text: TextSpan(
-          text: "Hai, ",
-          style: TextStyle(
-            fontSize: 18,
+          text: TextSpan(
+            text: "Hai, ",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+            children: [
+              TextSpan(
+                  text: "Muhammad",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ))
+            ],
           ),
-          children: [
-            TextSpan(
-                text: "Muhammad",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ))
-          ],
-        )),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print("klik qr");
+            },
+            child: Container(
+              margin: EdgeInsets.only(right: 20),
+              width: 30,
+              height: 30,
+              child: Image.asset(
+                "assets/icons/qrcode.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          )
+        ],
         backgroundColor: Color(0xFFEC2028),
         elevation: 0,
       ),
@@ -37,6 +52,156 @@ class HomeView extends GetView<HomeController> {
               height: 250,
               width: Get.width,
               color: Color(0xFFEC2028),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                Container(
+                    height: Get.height * 0.42,
+                    child: Column(
+                      children: [
+                        ClipPath(
+                          clipper: ClipInfoClass(),
+                          child: Container(
+                            padding: EdgeInsets.all(15),
+                            margin: EdgeInsets.symmetric(horizontal: 25),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color(0xFFE52D27),
+                                  Color(0xFFB31217),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "081290112333",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Image.asset("assets/logo/simpati.png")
+                                  ],
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  "Sisa Pulsa Anda",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Rp. 34,000",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 26,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      child: Text(
+                                        "Isi Pulsa",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Color(0xFFF7B731),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(height: 10),
+                                Divider(
+                                  color: Colors.black,
+                                ),
+                                SizedBox(height: 10),
+                                RichText(
+                                  text: TextSpan(
+                                    text: "Berlaku sampai",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: "19 April 2020",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Telkomsel POIN ",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 10,
+                                        vertical: 8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Color(0xFFF7B731),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Text(
+                                        "172",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(width: 10),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )),
+                Container(
+                  height: 10,
+                  color: Color(0xFFF1F2F6),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                )
+              ],
             ),
           )
         ],
@@ -53,6 +218,23 @@ class ClipPathClass extends CustomClipper<Path> {
     path.quadraticBezierTo(
         size.width / 2, size.height, size.width, size.height - 60);
 
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+class ClipInfoClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width - 65, size.height);
+    path.lineTo(size.width, size.height - 65);
     path.lineTo(size.width, 0);
     path.close();
 
