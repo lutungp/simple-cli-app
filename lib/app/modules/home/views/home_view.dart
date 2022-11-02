8fd9f972867019f4e61c066e788da35524bf2afe
lintang permana <lntngp19@gmail.com>
2022-11-03 01:08:53 +0700

    update status card

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -58,140 +58,161 @@ class HomeView extends GetView<HomeController> {
             margin: EdgeInsets.only(top: 20),
             child: Column(
               children: [
-                Container(
-                    height: Get.height * 0.42,
-                    child: Column(
-                      children: [
-                        ClipPath(
-                          clipper: ClipInfoClass(),
-                          child: Container(
-                            padding: EdgeInsets.all(15),
-                            margin: EdgeInsets.symmetric(horizontal: 25),
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color(0xFFE52D27),
-                                  Color(0xFFB31217),
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                Column(
+                  children: [
+                    ClipPath(
+                      clipper: ClipInfoClass(),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        margin: EdgeInsets.symmetric(horizontal: 25),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFE52D27),
+                              Color(0xFFB31217),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "081290112333",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    Image.asset("assets/logo/simpati.png")
-                                  ],
-                                ),
-                                SizedBox(height: 20),
                                 Text(
-                                  "Sisa Pulsa Anda",
+                                  "081290112333",
                                   style: TextStyle(
                                     color: Colors.white,
-                                    fontSize: 18,
+                                    fontSize: 20,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Rp. 34,000",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                                Image.asset("assets/logo/simpati.png")
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "Sisa Pulsa Anda",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Rp. 34,000",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Isi Pulsa",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
                                     ),
-                                    ElevatedButton(
-                                      onPressed: () {},
-                                      child: Text(
-                                        "Isi Pulsa",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color(0xFFF7B731),
-                                      ),
-                                    )
-                                  ],
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color(0xFFF7B731),
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Divider(
+                              color: Colors.black,
+                            ),
+                            SizedBox(height: 10),
+                            RichText(
+                              text: TextSpan(
+                                text: "Berlaku sampai",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
                                 ),
-                                SizedBox(height: 10),
-                                Divider(
-                                  color: Colors.black,
-                                ),
-                                SizedBox(height: 10),
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Berlaku sampai",
+                                children: [
+                                  TextSpan(
+                                    text: "19 April 2020",
                                     style: TextStyle(
                                       color: Colors.white,
                                       fontSize: 16,
+                                      fontWeight: FontWeight.bold,
                                     ),
-                                    children: [
-                                      TextSpan(
-                                        text: "19 April 2020",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      )
-                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Telkomsel POIN ",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
                                   ),
                                 ),
-                                SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Telkomsel POIN ",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                      ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 8,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFF7B731),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Text(
+                                    "172",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
                                     ),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: 10,
-                                        vertical: 8,
-                                      ),
-                                      decoration: BoxDecoration(
-                                        color: Color(0xFFF7B731),
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      child: Text(
-                                        "172",
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
-                                SizedBox(width: 10),
                               ],
                             ),
+                            SizedBox(width: 10),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          StatusCard(
+                            title: "Internet",
+                            data: "12.9",
+                            satuan: "GB",
                           ),
-                        )
-                      ],
-                    )),
+                          StatusCard(
+                            title: "Telpon",
+                            data: "0",
+                            satuan: "min",
+                          ),
+                          StatusCard(
+                            title: "SMS",
+                            data: "23",
+                            satuan: "sms",
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                  ],
+                ),
                 Container(
                   height: 10,
                   color: Color(0xFFF1F2F6),
@@ -205,6 +226,55 @@ class HomeView extends GetView<HomeController> {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class StatusCard extends StatelessWidget {
+  const StatusCard(
+      {Key? key, required this.title, required this.data, required this.satuan})
+      : super(key: key);
+
+  final String title;
+  final String data;
+  final String satuan;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        width: Get.width * 0.25,
+        height: 70,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                  text: data,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFEC2028),
+                  ),
+                  children: [
+                    TextSpan(
+                        text: " $satuan",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFF747D8C),
+                        ))
+                  ]),
+            )
+          ],
+        ),
       ),
     );
   }
